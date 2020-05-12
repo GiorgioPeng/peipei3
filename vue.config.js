40660926d1956827046363010e291054f623fea0
giorgiopeng <84867579@qq.com>
2020-05-12 19:50:14 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,10 @@ module.exports = {
     devServer: {
         disableHostCheck: true,
     },
+
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/peipei3/'
+        : '/',
     // 配置别名
     chainWebpack: (config) => {
         config.resolve.alias
