19aac7bc34249ee47e36395c55c65e44f99a705b
giorgiopeng <84867579@qq.com>
2020-05-12 12:56:24 +0800

    .

--- src/config/info.js
+++
@@ -1,5 +0,0 @@
-export const actionSheetOptions = {
-    "school": [{ name: "北京交通大学" }, { name: "北京邮电大学" }, { name: "北京科技大学" }, { name: "北京师范大学" }, { name: "北京理工大学" }],
-    "major": [{ name: "信管" }, { name: "计科" }, { name: "会计学" }, { name: "工商管理" }, { name: "数媒" },],
-    "career": [{ name: "金融" }, { name: "公务员" }, { name: "服务业" }, { name: "科研" }, { name: "软件开发" },],
-}
